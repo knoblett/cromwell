935b22865fd15e6fa4d7ccc0291d401165ecb032
Ruchi Munshi <rmunshi@broadinstitute.org>
2016-09-26 16:34:57 -0400

    change the find to help gotc build pass

--- docker/install.sh
+++ docker/install.sh
@@ -5,6 +5,6 @@ set -e
 CROMWELL_DIR=$1
 cd $CROMWELL_DIR
 sbt 'set test in assembly := {}' notests:assembly
-CROMWELL_JAR=$(find target | grep 'cromwell.*\.jar')
+CROMWELL_JAR=$(find engine/target | grep 'cromwell.*\.jar')
 mv $CROMWELL_JAR ./cromwell.jar
 sbt clean
